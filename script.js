c9b6c880932f8e64ec6485f911c359757ab91f50
laliahaidara <laliahaidarasadio@gmail.com>
2025-05-24 20:06:49 -0300

    Base 2 portfolio

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
 particlesJS("particles-js", {
-    "particles": {
-      "number": {
-        "value": 60,
-        "density": {
-          "enable": true,
-          "value_area": 800
-        }
-      },
-      "color": { "value": "#ffffff" },
-      "shape": {
-        "type": "circle",
-      },
-      "opacity": {
-        "value": 0.5,
-      },
-      "size": {
-        "value": 3,
-      },
-      "line_linked": {
-        "enable": true,
-        "distance": 150,
-        "color": "#ffb3e6",
-        "opacity": 0.4,
-        "width": 1
-      },
-      "move": {
-        "enable": true,
-        "speed": 3
-      }
+  particles: {
+    number: { value: 60, density: { enable: true, value_area: 800 } },
+    color: { value: "#ffffff" },
+    shape: { type: "circle" },
+    opacity: { value: 0.5 },
+    size: { value: 3 },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: "#fa69b5",
+      opacity: 0.4,
+      width: 1
     },
-    "interactivity": {
-      "events": {
-        "onhover": { "enable": true, "mode": "grab" }
-      },
-      "modes": {
-        "grab": { "distance": 140, "line_linked": { "opacity": 1 } }
-      }
-    },
-    "retina_detect": true
-  });
-  
+    move: { enable: true, speed: 2 }
+  },
+  interactivity: {
+    events: { onhover: { enable: true, mode: "grab" } },
+    modes: {
+      grab: { distance: 140, line_linked: { opacity: 1 } }
+    }
+  },
+  retina_detect: true
+});
+const toggleBtn = document.getElementById("toggleBtn");
+const maisTexto = document.getElementById("maisTexto");
+
+toggleBtn.addEventListener("click", () => {
+  if (maisTexto.style.display === "none") {
+    maisTexto.style.display = "inline";
+    toggleBtn.textContent = "Mostrar menos";
+  } else {
+    maisTexto.style.display = "none";
+    toggleBtn.textContent = "Leia mais";
+  }
+});
