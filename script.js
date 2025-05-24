15f55845048559685b1b2c551914a81ae3e9613c
laliahaidara <laliahaidarasadio@gmail.com>
2025-05-24 13:09:03 -0300

    Subindo base do portfólio

---
+++ script.js
@@ -0,0 +1,42 @@
+particlesJS("particles-js", {
+    "particles": {
+      "number": {
+        "value": 60,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": { "value": "#ffffff" },
+      "shape": {
+        "type": "circle",
+      },
+      "opacity": {
+        "value": 0.5,
+      },
+      "size": {
+        "value": 3,
+      },
+      "line_linked": {
+        "enable": true,
+        "distance": 150,
+        "color": "#ffb3e6",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 3
+      }
+    },
+    "interactivity": {
+      "events": {
+        "onhover": { "enable": true, "mode": "grab" }
+      },
+      "modes": {
+        "grab": { "distance": 140, "line_linked": { "opacity": 1 } }
+      }
+    },
+    "retina_detect": true
+  });
+  
